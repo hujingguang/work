810d6e99cbce0319bd6455f93fbd501747b3e11e
hoo <hoo@wikiki.cn>
2015-12-23 11:05:07 +0800

    modify py

--- shell/install_php_tengin.py
+++ shell/install_php_tengin.py
@@ -46,7 +46,7 @@ def compile_php():
     if res!=0:
         os.system("echo 'get php source tarbar failed' >%s" %LOG_FILE)
         exit()
-    configure_args='''--prefix=%s/php --enable-fpm --with-fpm-user=%s --with-fpm-group=www --with-config-file-path=/webdata/opt/local/php/etc --disable-ipv6 --with-pcre-regex --with-zlib --enable-calendar --enable-gd-native-ttf --with-freetype-dir --with-mysql --with-mysqli --with-pdo-mysql --enable-sockets --enable-soap --enable-mysqlnd --enable-mbstring ''' %(SOFT_DIR,PHP_Run_User)
+    configure_args='''--prefix=%s/php --enable-fpm --with-fpm-user=%s --with-fpm-group=%s --with-config-file-path=%s/php/etc --disable-ipv6 --with-pcre-regex --with-zlib --enable-calendar --enable-gd-native-ttf --with-freetype-dir --with-mysql --with-mysqli --with-pdo-mysql --enable-sockets --enable-soap --enable-mysqlnd --enable-mbstring ''' %(SOFT_DIR,PHP_Run_User,PHP_Run_User,SOFT_DIR)
     cmd=''' cd /tmp/php-%s && ./configure %s &>/dev/null && make &>/dev/null  && make install &>/dev/null ''' %(PHP_Version,configure_args)
     res=os.system(cmd)
     if res !=0:
@@ -55,7 +55,7 @@ def compile_php():
     res=os.system(r"egrep '^%s' /etc/passwd" %PHP_Run_User)
     if res!=0:
         os.system('useradd %s -s /sbin/nologin' %PHP_Run_User)
-
+    os.system("cp %s/php/etc/php-fpm.conf.default %/php/etc/php-fpm.conf " %(SOFT_DIR,SOFT_DIR))
 def compile_tengine():
     configure_args=''' --prefix=%s/tengine --with-jemalloc --with-jemalloc=/tmp/jemalloc-%s --with-http_ssl_module --user=%s --group=%s --with-pcre --with-http_spdy_module --with-http_upstream_session_sticky_module=shared ''' %(SOFT_DIR,Jemalloc_Version,Tengine_Run_User,Tengine_Run_User)
     cmd=''' wget http://web.wikiki.cn/jemalloc-%s.tar.bz2 &>/dev/null && mv jemalloc-%s.tar.bz2 /tmp && cd /tmp && tar -xjf jemalloc-%s.tar.bz2 ''' %(Jemalloc_Version,Jemalloc_Version,Jemalloc_Version)
@@ -77,6 +77,143 @@ def compile_tengine():
     if res!=0:
         os.system('useradd %s -s /sbin/nologin' %Tengine_Run_User)
 
+
+def generate_deamon_scripts():
+    php_script_contents=''' 
+    #!/bin/bash
+    PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
+    export PATH
+    pid_dir=%s/php/var/run
+    . /etc/sysconfig/network
+    . /etc/init.d/functions
+    [ "$NETWORKING" = "no" ] && exit 0
+    php_fpm="%s/php/sbin/php-fpm"
+    PHP_CONF_FILE=%s/php/etc/php.ini
+    prog=$(basename $php_fpm)
+    lockfile=/var/lock/subsys/php-fpm.lock
+    php_fpm_pid="%s/php/var/run/php-fpm.pid"
+    ulimit -SHn 65535 
+    start() {
+            [ -x $php_fpm ] || exit 1
+            [ -f $PHP_CONF_FILE ] || exit 2
+            echo -n $"Starting $prog: "
+            daemon --pidfile $php_fpm_pid $php_fpm -c $PHP_CONF_FILE
+            retval=$?
+            [ $retval -eq 0 ] && touch $lockfile
+            return $retval
+            }
+    php_fpm_status() {
+            status -p $php_fpm_pid $prog
+            }
+    stop() {
+            echo -n $"Stopping $prog: "
+            killproc -p $php_fpm_pid $prog -QUIT
+            sleep 5
+            retval=$?
+            echo
+            [ $retval -eq 0 ] && rm -f $lockfile
+            return $retval
+            }
+    restart() {
+            stop
+            start
+            }
+    case "$1" in
+    start)
+    start
+    ;;
+    stop)
+    stop
+    ;;
+    restart)
+    restart
+    ;;
+    status)
+    php_fpm_status
+    ;;
+    *)
+    echo $"Usage: $0 {start|stop|status|restart}"
+    exit 2
+    esac
+    ''' %(SOFT_DIR,SOFT_DIR,SOFT_DIR,SOFT_DIR)
+
+    nginx_script_contents=''' 
+    #!/bin/bash
+    . /etc/rc.d/init.d/functions
+    . /etc/sysconfig/network
+    [ "$NETWORKING" = "no" ] && exit 0
+    nginx="%s/tengine/sbin/nginx"
+    prog=$(basename $nginx)
+    NGINX_CONF_FILE="%s/tengine/conf/nginx.conf"
+    lockfile=/var/lock/subsys/nginx.lock
+    nginx_pid="%s/tengine/logs/nginx.pid"
+    start() {
+    [ -x $nginx ] || exit 5
+    [ -f $NGINX_CONF_FILE ] || exit 6
+    echo -n $"Starting $prog: "
+    daemon --pidfile $nginx_pid $nginx -c $NGINX_CONF_FILE
+    retval=$?
+    [ $retval -eq 0 ] && touch $lockfile
+    return $retval
+    }
+    stop() {
+    echo -n $"Stopping $prog: "
+    killproc -p $nginx_pid $prog
+    retval=$?
+    [ $retval -eq 0 ] && rm -f $lockfile
+    return $retval
+    }
+    restart() {
+    stop
+    start
+    }
+    reload() {
+    echo -n $"Reloading $prog: "
+    $nginx -s reload
+    RETVAL=$?
+    return $RETVAL
+    }
+    nginx_status() {
+    status $prog
+    }
+    case "$1" in
+    start)
+    start
+    ;;
+    stop)
+    stop
+    ;;
+    restart)
+    restart
+    ;;
+    reload)
+    reload
+    ;;
+    status)
+    nginx_status
+    ;;
+    *)
+    echo $"Usage: $0 {start|stop|status|restart|reload}"
+    exit 2
+    esac
+    ''' %(SOFT_DIR,SOFT_DIR,SOFT_DIR)
+
+    if not os.path.exists("/etc/init.d/php-fpm"):
+        php_fpm_script=open("/etc/init.d/php-fpm","w")
+        php_fpm_script.write(php_script_contents)
+        php_fpm_script.close()
+        res=os.system("chmod +x /etc/init.d/php-fpm && chkconfig --add php-fpm && chkconfig --level 35 php-fpm on ")
+        if res!=0:
+            os.system("echo 'add php-fpm deamon failed ' >> /tmp/install.log")
+    if not os.path.exists("/etc/init.d/nginx"):
+        nginx_script=open("/etc/init.d/nginx","w")
+        nginx_script=write(nginx_script_contents)
+        nginx_script=close()
+        res=os.system("chmod +x /etc/init.d/nginx && chkconfig --add nginx && chkconfig --level 35 nginx on ")
+        if res!=0:
+            os.system("echo 'add nginx deamon faild ' >>/tmp/install.log ")
+
+
 def start_install():
     check_network()
     install_epel()
@@ -89,6 +226,7 @@ def start_install():
     if not os.path.exists('%s/tengine' %SOFT_DIR):
         compile_tengine()
     print "Ok .....  php and tengine is installed "
+    generate_deamon_scripts()
 
 if __name__=="__main__":
     start_install()
